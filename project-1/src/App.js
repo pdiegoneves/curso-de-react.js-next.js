8c091e7a3fa021f7c91ac9eeb9783a67c448e6a1
Diego Neves <pdcassiano@gmail.com>
2022-12-02 15:52:11 -0300

    método map

--- project-1/src/App.js
+++ project-1/src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    posts: [
+      {
+        id: 1,
+        title: 'O título 1',
+        body: 'O corpo 1',
+      },
+      {
+        id: 2,
+        title: 'O título 2',
+        body: 'O corpo 2',
+      },
+      {
+        id: 3,
+        title: 'O título 3',
+        body: 'O corpo 3',
+      },
+    ],
+  }
+
+  render() {
+    const { posts } = this.state
+
+    return (
+      <div className="App">
+        {posts.map((post) => (
+          <div>
+            <h1>{post.title}</h1>
+            <p>{post.body}</p>
+          </div>
+        ))}
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
