18e12e5c53acc0c06b1e890a38a138b19ad4910f
Diego Neves <pdcassiano@gmail.com>
2023-01-02 17:57:05 -0300

    use memo

--- project-2/src/App.js
+++ project-2/src/App.js
@@ -1,18 +1,32 @@
-import logo from './logo.svg'
+import P from 'prop-types'
 import './App.css'
+import React, { useCallback, useMemo, useState } from 'react'
+
+const Button = ({ incrementButton }) => {
+  console.log('filho renderizou')
+
+  return <button onClick={() => incrementButton(100)}>+</button>
+}
+
+Button.propTypes = {
+  incrementButton: P.func,
+}
 
 function App() {
+  const [counter, setCounter] = useState(0)
+
+  const incrementCounter = useCallback((num) => {
+    setCounter((c) => c + num)
+  }, [])
+
+  console.log('pai renderizou')
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
+      <p>Test 3</p>
+      <h1>C1: {counter}</h1>
+      {useMemo(() => {
+        return <Button incrementButton={incrementCounter} />
+      }, [incrementCounter])}
     </div>
   )
 }
