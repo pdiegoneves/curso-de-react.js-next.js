6baac0b4183c1a2542e0ff61d96e566642745b5d
Diego Neves <pdcassiano@gmail.com>
2023-01-09 12:57:40 -0300

    started useContex

--- project-2/src/App.js
+++ project-2/src/App.js
@@ -1,10 +1,41 @@
+import React, { useContext } from 'react'
 import './App.css'
+
+const globalState = {
+  title: 'o titulo do contexto',
+  body: 'o body do contexto',
+  counter: 0,
+}
+
+const GlobalContext = React.createContext()
+
+//eslint-disable-next-line
+const Div = ({ children }) => {
+  return (
+    <>
+      <H1 />
+      <P />
+    </>
+  )
+}
+
+//eslint-disable-next-line
+const H1 = () => {
+  const theContext = useContext(GlobalContext)
+  return <h1>{theContext.title}</h1>
+}
+
+//eslint-disable-next-line
+const P = () => {
+  const theContext = useContext(GlobalContext)
+  return <p>{theContext.body}</p>
+}
 
 function App() {
   return (
-    <div className="App">
-      <h1>Oi</h1>
-    </div>
+    <GlobalContext.Provider value={globalState}>
+      <Div />
+    </GlobalContext.Provider>
   )
 }
 
