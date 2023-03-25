4ee0409ba6ae01777d3a8cc60c7feaac633c3294
Diego Neves <pdcassiano@gmail.com>
2023-03-25 20:22:52 -0300

    ok

--- project-1/src/templates/Home/index.jsx
+++ project-1/src/templates/Home/index.jsx
@@ -1,25 +1,45 @@
 import { useState, useEffect, useCallback } from 'react'
 
-const useAsync = () => {
+const useAsync = (asyncFunction, shouldRun) => {
   const [result, setResult] = useState(null)
   const [error, setError] = useState(null)
   const [status, setStatus] = useState('idle')
 
-  const run = useCallback(() => {}, [])
+  const run = useCallback(() => {
+    console.log('EFFECT', new Date().toLocaleTimeString())
+    setResult(null)
+    setError(null)
+    setStatus('pending')
+
+    return asyncFunction()
+      .then((response) => {
+        setStatus('settled')
+        setResult(response)
+      })
+      .catch((error) => {
+        setStatus('error')
+        setError(error)
+      })
+  }, [asyncFunction])
+
+  useEffect(() => {
+    if (shouldRun) {
+      run()
+    }
+  }, [run, shouldRun])
+
+  return [run, result, error, status]
+}
+
+const fetchData = async () => {
+  const data = await fetch('https://jsonplaceholder.typicode.com/posts/')
+  const json = await data.json()
+  return json
 }
 
 export const Home = () => {
   const [posts, setPosts] = useState(null)
+  const [reFetchData, result, error, status] = useAsync(fetchData, true)
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const data = await fetch('https://jsonplaceholder.typicode.com/posts/')
-      const json = await data.json()
-      setPosts(json)
-      return json
-    }
-
-    fetchData()
-  }, [])
-  return <pre>{JSON.stringify(posts, null, 2)}</pre>
+  return <pre>{JSON.stringify(result, null, 2)}</pre>
 }
