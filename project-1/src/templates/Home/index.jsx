a8b81e6c500fac85d34c20a92faf148ba76fb136
Diego Neves <pdcassiano@gmail.com>
2023-04-14 05:44:16 -0300

    useDebugValue

--- project-1/src/templates/Home/index.jsx
+++ project-1/src/templates/Home/index.jsx
@@ -1,48 +1,39 @@
-import { forwardRef, useEffect, useImperativeHandle, useRef, useState } from 'react'
+import { useDebugValue, useEffect, useState } from 'react'
 
-export const Home = () => {
-  const [counted, setCounted] = useState([0, 1, 2, 3, 4])
+const useMediaQuery = (queryValue, initialValues = false) => {
+  const [match, setMatch] = useState(initialValues)
 
-  const divRef = useRef()
-
-  // useLayoutEffect(() => {
-  useEffect(() => {
-    divRef.current.divRef.scrollTop = divRef.current.divRef.scrollHeight
+  useDebugValue(`Query: ${queryValue}`, (name) => {
+    return name + ' modificado'
   })
 
-  const handleClick = () => {
-    setCounted((c) => [...c, +c.slice(-1) + 1])
-    divRef.current.handleClick()
-  }
-  return (
-    <>
-      <button onClick={handleClick}>Count {counted.slice(-1)}</button>
-      <DisplayCounted counted={counted} ref={divRef} />
-    </>
-  )
+  useEffect(() => {
+    let isMounted = true
+    const matchMedia = window.matchMedia(queryValue)
+
+    const handleChange = () => {
+      if (!isMounted) return
+
+      setMatch(!!matchMedia.matches)
+    }
+
+    matchMedia.addEventListener('change', handleChange)
+    setMatch(!!matchMedia.matches)
+
+    return () => {
+      isMounted = false
+      matchMedia.removeEventListener('change', handleChange)
+    }
+  }, [queryValue])
+
+  return match
 }
 
-export const DisplayCounted = forwardRef(function DisplayCounted({ counted }, ref) {
-  const [rand, setRand] = useState('0.24')
-  const divRef = useRef()
-  const handleClick = () => {
-    setRand(Math.random().toFixed(2))
-  }
-
-  useImperativeHandle(ref, () => ({
-    handleClick,
-    divRef: divRef.current,
-  }))
-
-  return (
-    <div ref={divRef} style={{ height: '100px', width: '200px', overflowY: 'scroll' }}>
-      {counted.map((c) => {
-        return (
-          <p onClick={handleClick} key={`c-${c}`}>
-            {c} +++ {rand}
-          </p>
-        )
-      })}
-    </div>
-  )
-})
+export const Home = () => {
+  const huge = useMediaQuery('(min-width: 980px)')
+  const big = useMediaQuery('(max-width: 979px) and (min-width: 768px)')
+  const medium = useMediaQuery('(max-width: 767px) and (min-width: 321px)')
+  const small = useMediaQuery('(max-width: 321px)')
+  const background = huge ? 'green' : big ? 'red' : medium ? 'yellow' : small ? 'purple' : null
+  return <div style={{ fontSize: '60px', background }}>OI</div>
+}
