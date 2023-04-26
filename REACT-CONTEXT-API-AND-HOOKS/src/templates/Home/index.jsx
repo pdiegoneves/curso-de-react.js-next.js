fdb7db91bbff52d3c9e13c23535b4051d961fcf1
Diego Neves <pdcassiano@gmail.com>
2023-04-26 13:06:05 -0300

    react context api end

--- REACT-CONTEXT-API-AND-HOOKS/src/templates/Home/index.jsx
+++ REACT-CONTEXT-API-AND-HOOKS/src/templates/Home/index.jsx
@@ -6,11 +6,28 @@ import { Heading } from '../../components/Heading'
 export const Home = () => {
   const [state, actions] = useCounterContext()
 
+  const handleError = async () => {
+    actions
+      .asyncError()
+      .then((r) => console.log(r))
+      .catch((e) => console.log(e.name, ':', e.message))
+  }
+
   return (
     <div>
       <Heading />
       <div>
-        <Button onButtonClick={actions.increase}>Increase</Button>
+        <Button onButtonClick={actions.increase}>increase</Button>
+        <Button onButtonClick={actions.decrease}>decrease</Button>
+        <Button onButtonClick={actions.reset}>reset</Button>
+        <Button onButtonClick={() => actions.setCounter({ counter: 10 })}>Set 10</Button>
+        <Button onButtonClick={() => actions.setCounter({ counter: 100 })}>Set 100</Button>
+        <Button disabled={state.loading} onButtonClick={actions.asyncIncrease}>
+          Async Increase
+        </Button>
+        <Button disabled={state.loading} onButtonClick={handleError}>
+          Async Error
+        </Button>
       </div>
     </div>
   )
