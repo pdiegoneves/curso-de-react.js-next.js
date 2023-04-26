2483cdddef961b3e2382f85b834a0c03c94e7eaf
Diego Neves <pdcassiano@gmail.com>
2023-04-26 06:00:36 -0300

    React context api
    Criado Button e Heading

--- REACT-CONTEXT-API-AND-HOOKS/src/templates/Home/index.jsx
+++ REACT-CONTEXT-API-AND-HOOKS/src/templates/Home/index.jsx
@@ -1,16 +1,17 @@
 import { useCounterContext } from '../../contexts/CounterContext'
 import { useEffect, useState } from 'react'
+import { Button } from '../../components/Button'
+import { Heading } from '../../components/Heading'
 
 export const Home = () => {
   const [state, actions] = useCounterContext()
 
-  useEffect(() => {
-    actions.increase()
-  }, [actions])
-
   return (
     <div>
-      <h1 onClick={() => actions.increase()}>Oi</h1>
+      <Heading />
+      <div>
+        <Button onButtonClick={actions.increase}>Increase</Button>
+      </div>
     </div>
   )
 }
