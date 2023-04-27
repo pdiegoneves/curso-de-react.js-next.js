f8bc35f76059aed18bf1e4d54413426cee83d9a9
Diego Neves <pdcassiano@gmail.com>
2023-04-27 05:16:19 -0300

    useHistory

--- project-2/src/templates/Abc/index.jsx
+++ project-2/src/templates/Abc/index.jsx
@@ -1,11 +1,24 @@
-import { useParams } from 'react-router-dom'
+import { useEffect } from 'react'
+import { useParams, useLocation, useHistory } from 'react-router-dom'
 
 export const Abc = () => {
   const { slug, id } = useParams()
+  const location = useLocation()
+  const history = useHistory()
+
+  console.log(location)
+  console.log(history)
+
+  useEffect(() => {
+    setTimeout(() => {
+      history.push('/')
+    }, 5000)
+  }, [])
+
   return (
     <div>
       <h1>
-        ABC {slug} {id}{' '}
+        ABC {slug} {id}
       </h1>
     </div>
   )
--- project-2/src/templates/App/index.jsx
+++ project-2/src/templates/App/index.jsx
@@ -1,9 +1,18 @@
+import { useEffect } from 'react'
 import { Posts } from '../../components/Posts'
 import { CounterProvider } from '../../contexts/CounterProvider'
 import { PostsProvider } from '../../contexts/PostsProvider'
 import './styles.css'
+import { useHistory } from 'react-router-dom'
 
 export function App() {
+  const history = useHistory()
+
+  useEffect(() => {
+    setTimeout(() => {
+      history.push('/abc')
+    }, 5000)
+  }, [])
   return (
     <CounterProvider>
       <PostsProvider>
