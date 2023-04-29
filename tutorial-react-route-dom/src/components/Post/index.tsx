a814ea06a357dd9577168d15654aece0b4928c18
Diego Neves <pdcassiano@gmail.com>
2023-04-29 19:45:08 -0300

    Outloet e rotas aninhadas

--- tutorial-react-route-dom/src/components/Post/index.tsx
+++ tutorial-react-route-dom/src/components/Post/index.tsx
@@ -3,6 +3,7 @@ import './style.css'
 export const Post = () => {
     return (
         <div>
+            <h1>Post</h1>
             <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae nostrum sequi possimus rem architecto molestias, ea eius earum reiciendis voluptas nobis unde accusantium labore, animi iste omnis amet temporibus adipisci.</p>
         </div>
     )
